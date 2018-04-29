2ceb3b223aecdfe550524f03dd5092eeae31e7dc
ru iz <ruhaim@gmail.com>
2018-04-30 01:45:36 +0530

    basic eslint and prettify cleanup

--- App/Components/LocationProgessiveInput.js
+++ App/Components/LocationProgessiveInput.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import ProgressiveInput from './ProgressiveInput';
 
 export default class LocationProgessiveInput extends Component {
@@ -7,28 +7,29 @@ export default class LocationProgessiveInput extends Component {
 
     this.state = {
       value: '',
-      isLoading: false
+      isLoading: false,
     };
   }
-  
+
   _onChangeText(text) {
-    this.setState({isLoading: true, value: text});
-    
-    fetch("https://www.metaweather.com/api/location/search/?query="+text)
-      .then((result) => {
-        // Process list of suggestions
-        
-        this.setState({isLoading: false});
-      });
+    this.setState({ isLoading: true, value: text });
+
+    fetch(`https://www.metaweather.com/api/location/search/?query=${text}`).then((result) => {
+      // Process list of suggestions
+
+      this.setState({ isLoading: false });
+    });
   }
 
   render() {
-    return (<ProgressiveInput
-      value={this.state.value}
-      isLoading={this.state.isLoading}
-      onChangeText={this._onChangeText.bind(this)}
-    />);
+    return (
+      <ProgressiveInput
+        value={this.state.value}
+        isLoading={this.state.isLoading}
+        onChangeText={(event) => {
+          this._onChangeText(event);
+        }}
+      />
+    );
   }
 }
-
-
--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,21 @@
-import { StackNavigator } from 'react-navigation'
-import LaunchScreen from '../Containers/LaunchScreen'
+import { StackNavigator } from 'react-navigation';
+import LaunchScreen from '../Containers/LaunchScreen';
 
-import styles from './Styles/NavigationStyles'
+import styles from './Styles/NavigationStyles';
 
 // Manifest of possible screens
-const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
-}, {
-  // Default config for all screens
-  headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
-  navigationOptions: {
-    headerStyle: styles.header
-  }
-})
+const PrimaryNav = StackNavigator(
+  {
+    LaunchScreen: { screen: LaunchScreen },
+  },
+  {
+    // Default config for all screens
+    headerMode: 'none',
+    initialRouteName: 'LaunchScreen',
+    navigationOptions: {
+      headerStyle: styles.header,
+    },
+  },
+);
 
-export default PrimaryNav
+export default PrimaryNav;
