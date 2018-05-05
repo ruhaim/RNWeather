0858e5d99381780fe85de34ef94f21d19e610505
Ruhaim <ruhaim@gmail.com>
2018-05-05 07:31:02 +0530

    Added forecast detail view

--- App/Components/ForecastComponents/ForecastDayItem.js
+++ App/Components/ForecastComponents/ForecastDayItem.js
@@ -65,8 +65,8 @@ export default class ForecastComponent extends Component {
             </View>
 
             <Text style={{ marginBottom: 10 }}>
-              The idea with React Native Elements is more about component structure than actual
-              design.
+              The idea with React Native Elements is more about component
+              structure than actual design.
             </Text>
 
             <Button
